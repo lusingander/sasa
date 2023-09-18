a96d861ebb218bd1b65b1fd6fb2ee671129dfa8c
Kyosuke Fujimoto <kyoro.f@gmail.com>
2023-09-18 15:37:53 +0900

    Add comments to public functions

--- sasa.go
+++ sasa.go
@@ -12,12 +12,16 @@ type trimMarginOptions struct {
 
 type trimMarginOption func(*trimMarginOptions)
 
+// MarginPrefix is an option that specifies the string to use as a delimiter for TrimMargin.
 func MarginPrefix(s string) trimMarginOption {
 	return func(opt *trimMarginOptions) {
 		opt.marginPrefix = s
 	}
 }
 
+// TrimMargin returns a string with leading whitespace removed up to the delimiter.
+// The delimiter can be specified with the MarginPrefix option (default: "|").
+// If the first and last blank lines are blank, those lines are also removed.
 func TrimMargin(s string, options ...trimMarginOption) string {
 	opts := &trimMarginOptions{
 		marginPrefix: "|",
@@ -54,6 +58,8 @@ func TrimMargin(s string, options ...trimMarginOption) string {
 	return strings.Join(ss, "\n")
 }
 
+// TrimIndent returns a string with leading whitespace characters removed by the minimum number of whitespace characters on each line.
+// If the first and last blank lines are blank, those lines are also removed.
 func TrimIndent(s string) string {
 	lines := strings.Split(s, "\n")
 	ln := len(lines)
